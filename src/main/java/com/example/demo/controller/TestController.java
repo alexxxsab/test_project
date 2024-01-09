d5b1f8fe9185bacf872054bf21305a2d2ac93f73
sabu <saburov@darasa.io>
2024-01-09 10:13:30 +0400

    init

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @GetMapping
+    public String getTest(){
+        return "GET OK";
+    }
+
+    @PostMapping
+    public String postTest(){
+        return "POST OK";
+    }
+}
